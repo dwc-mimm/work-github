207e572626752b0cb0a2069a53ca44873d8085c4
EC2 Default User <ec2-user@ip-172-31-20-45.ec2.internal>
2021-07-15 09:57:28 +0000

    [add]productsのMVC追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root to: 'public/products#top'
+  get '/about' => 'public/homes#about', as: 'about'
+
+  scope module: :public do
+    resources :products, only: [:index, :show]
+  end
+
 end
