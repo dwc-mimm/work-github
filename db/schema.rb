29d913d798965dbbc5e76fce5cb30087a8022d4b
iinowataru <wpfujz@gmail.com>
2021-07-15 10:22:39 +0000

    orders

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20210715100608) do
+ActiveRecord::Schema.define(version: 20210715101334) do
 
   create_table "admin_products", force: :cascade do |t|
     t.integer  "genre_id",           null: false
@@ -21,6 +21,19 @@ ActiveRecord::Schema.define(version: 20210715100608) do
     t.boolean  "sale_status",        null: false
     t.datetime "created_at",         null: false
     t.datetime "updated_at",         null: false
+  end
+
+  create_table "orders", force: :cascade do |t|
+    t.integer  "customer_id",    null: false
+    t.integer  "postage",        null: false
+    t.integer  "payment",        null: false
+    t.integer  "payment_method", null: false
+    t.string   "name",           null: false
+    t.string   "postcode",       null: false
+    t.string   "address",        null: false
+    t.integer  "order_status",   null: false
+    t.datetime "created_at",     null: false
+    t.datetime "updated_at",     null: false
   end
 
 end
