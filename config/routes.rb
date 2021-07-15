fb54f3bc9dfe93f356db20ed120572eb6de8d1c0
EC2 Default User <ec2-user@ip-172-31-20-45.ec2.internal>
2021-07-15 13:29:32 +0000

    [add]deviseを用いたpublic側のMVC追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  devise_for :customers, :controllers => {
+    :sessions => 'customers/sessions',
+    :registrations => 'customers/registrations',
+    :passwords => 'customers/passwords'
+  }
 
   root to: 'public/products#top'
-  get '/about' => 'public/homes#about', as: 'about'
+  get '/about' => 'public/products#about', as: 'about'
 
   scope module: :public do
     resources :products, only: [:index, :show]
