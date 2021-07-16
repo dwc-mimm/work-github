48888dd39feb91b3b8edef96863e4c7aa7018195
iinowataru <wpfujz@gmail.com>
2021-07-16 11:24:25 +0000

    install-orders

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,29 @@
 
 ActiveRecord::Schema.define(version: 20210715101334) do
 
+  create_table "admin_products", force: :cascade do |t|
+    t.integer  "genre_id",          null: false
+    t.string   "image_id",          null: false
+    t.string   "name",              null: false
+    t.text     "description",       null: false
+    t.integer  "tax_exclued_price", null: false
+    t.boolean  "sale_status",       null: false
+    t.datetime "created_at",        null: false
+    t.datetime "updated_at",        null: false
+  end
+
+  create_table "admins", force: :cascade do |t|
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
+    t.index ["email"], name: "index_admins_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
+  end
+
   create_table "orders", force: :cascade do |t|
     t.integer  "customer_id",    null: false
     t.integer  "postage",        null: false
