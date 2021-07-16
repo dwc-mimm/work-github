9d0253a2205a5e0aae70d3269a44a1234f1db9b3
ranranluu <ranranluutennis@gmail.com>
2021-07-16 13:07:25 +0000

    [add]adminのorder_productsのMVCファイル追加

---
+++ app/controllers/admin/order_products_controller.rb
@@ -0,0 +1,11 @@
+class Admin::OrderProductsController < ApplicationController
+
+def update
+end
+
+private
+def order_product_params
+  params.require(:order_products).permit(:production_status)
+  
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -8,4 +8,7 @@ Rails.application.routes.draw do
     resources :products, only: [:index, :show]
   end
 
+  namespace :admin do
+    resources :order_products, only: [:update]
+  end
 end
