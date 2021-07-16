44fd539d9b5e02ee1b0a1b5666abd1f2d3315fe0
ranranluu <ranranluutennis@gmail.com>
2021-07-16 12:38:15 +0000

    [add]adminの genresのMVCファイル追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  namespace :admin do
+    resources :genres, only: [:index, :create, :edit, :update]
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root to: 'public/products#top'
