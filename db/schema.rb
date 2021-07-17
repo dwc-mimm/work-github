eccbfa41e7caae5be812c6fc6b5d7bdb14cea750
ranranluu <ranranluutennis@gmail.com>
2021-07-17 03:21:50 +0000

    [add]publicのcartsのMVC追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,23 +10,28 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 2021_07_16_124859) do
-
+ActiveRecord::Schema.define(version: 2021_07_17_024037) do
 
   create_table "admins", force: :cascade do |t|
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
-    t.string   "reset_password_token"
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.datetime "created_at",                          null: false
-    t.datetime "updated_at",                          null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["email"], name: "index_admins_on_email", unique: true
     t.index ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
   end
 
-  
+  create_table "carts", force: :cascade do |t|
+    t.integer "product_id", null: false
+    t.integer "customer_id", null: false
+    t.integer "product_quantity", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "customers", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -47,7 +52,6 @@ ActiveRecord::Schema.define(version: 2021_07_16_124859) do
     t.index ["reset_password_token"], name: "index_customers_on_reset_password_token", unique: true
   end
 
-
   create_table "deliveries", force: :cascade do |t|
     t.integer "customer_id", null: false
     t.string "name"
@@ -57,13 +61,11 @@ ActiveRecord::Schema.define(version: 2021_07_16_124859) do
     t.datetime "updated_at", null: false
   end
 
-  
   create_table "genres", force: :cascade do |t|
     t.string "name", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
-
 
   create_table "order_products", force: :cascade do |t|
     t.integer "product_id", null: false
@@ -75,7 +77,6 @@ ActiveRecord::Schema.define(version: 2021_07_16_124859) do
     t.datetime "updated_at", null: false
   end
 
-  
   create_table "orders", force: :cascade do |t|
     t.integer "customer_id", null: false
     t.integer "postage", default: 800, null: false
@@ -89,7 +90,6 @@ ActiveRecord::Schema.define(version: 2021_07_16_124859) do
     t.datetime "updated_at", null: false
   end
 
-
   create_table "products", force: :cascade do |t|
     t.integer "genre_id", null: false
     t.string "image_id", null: false
@@ -99,8 +99,6 @@ ActiveRecord::Schema.define(version: 2021_07_16_124859) do
     t.boolean "sale_status", default: true, null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-
-
   end
 
 end
