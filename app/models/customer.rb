26ff3a6603444ba7b3cb02801b89fdd5a1f90cc8
iinowataru <wpfujz@gmail.com>
2021-07-18 02:24:19 +0000

    develop

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -9,7 +9,8 @@ class Customer < ApplicationRecord
   has_many :deliveries, dependent: :destroy
   has_many :orders
 
-  validates :last_name, :first_name, :last_name_kana, :first_name_kana, :postcode, :address, :phone_number, presence: true
-
+  validates :last_name, :first_name, :last_name_kana, :first_name_kana, :postcode, :address, :phone_number, :is_deleted, presence: true
+  enum is_deleted: { customer: false, deleted_customer: true }
+  #boolean箇所正しい記述か不明です
 
 end
