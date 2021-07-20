c1d50a2c2658151f3f7f4b49904146e43ddc8f2a
koichimatz <koichi.matsumoto@gmail.com>
2021-07-20 06:54:22 +0000

    [Add2]header途中

--- config/routes.rb
+++ config/routes.rb
@@ -2,24 +2,29 @@ Rails.application.routes.draw do
 
   namespace :admin do
     resources :genres, only: [:index, :create, :edit, :update]
+    resources :customers, only: [:index, :show, :edit, :update]
   end
 
   devise_for :admins
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  devise_for :customers, :controllers => {
-    :sessions => 'customers/sessions',
-    :registrations => 'customers/registrations',
-    :passwords => 'customers/passwords'
-  }
+  devise_for :customers
+  # :controllers => {
+  #   :sessions => 'customers/sessions',
+  #   :registrations => 'customers/registrations',
+  #   :passwords => 'customers/passwords'
+  # }
 
   root to: 'public/products#top'
   get '/about' => 'public/products#about', as: 'about'
-
+  get 'customers/quit' => 'public/customers#quit', as: 'quit'
+  patch 'customers/withdraw' => 'public/customers#withdraw', as: 'withdraw'
+  
   scope module: :public do
     resources :products, only: [:index, :show]
     resources :deliveries, only: [:index, :create, :edit, :update, :destroy]
-    resources :carts, only: [:index, :create, :update, :destroy] do
+    resources :carts, only: [:index, :create, :update, :destroy] 
+    resources :customers, only: [:show, :edit, :update] do
       collection do
         delete '/' => 'carts#destroy_all'
       end
@@ -29,6 +34,4 @@ Rails.application.routes.draw do
   namespace :admin do
     resources :order_products, only: [:update]
   end
-
-
 end
