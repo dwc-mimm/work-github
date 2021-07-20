7f92a61aa0564a32b5669a968cf9db4e05ae5400
iinowataru <wpfujz@gmail.com>
2021-07-20 06:54:25 +0000

    order作成中です

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -1,18 +1,25 @@
 class Public::OrdersController < ApplicationController
     
  def new
+  @order = Order.new
+  @customer = current_customer
  end
  
  def confirm
+
  end
  
  def complete
  end
  
  def create
+  @order = Order.new
+  @order.save
+  redirect_to confirm_path
  end
  
  def index
+  @orders = current_customer.orders
  end
  
  def show
--- config/routes.rb
+++ config/routes.rb
@@ -7,16 +7,21 @@ Rails.application.routes.draw do
   devise_for :admins
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  devise_for :customers, :controllers => {
-    :sessions => 'customers/sessions',
-    :registrations => 'customers/registrations',
-    :passwords => 'customers/passwords'
-  }
+  devise_for :customers
+  #, :controllers 
+  #=> {
+    #:sessions => 'customers/sessions',
+    #:registrations => 'customers/registrations',
+    #:passwords => 'customers/passwords'
+  #}
 
   root to: 'public/products#top'
   get '/about' => 'public/products#about', as: 'about'
-
+  get '/confirm' => 'public/orders#confirm', as: 'confirm'
+  get '/complete' => 'public/orders#complete', as: 'complete'
+  
   scope module: :public do
+    resources :orders, only: [:new, :create, :index, :show]
     resources :products, only: [:index, :show]
     resources :deliveries, only: [:index, :create, :edit, :update, :destroy]
     resources :carts, only: [:index, :create, :update, :destroy] do
