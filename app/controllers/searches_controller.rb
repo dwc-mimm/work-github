73ed9a2c2fce9ca14712b7f1288f28f134f1cfe4
ranranluu <ranranluutennis@gmail.com>
2021-07-24 12:42:47 +0000

    [fix]public側のジャンル検索と商品検索

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -2,37 +2,42 @@ class SearchesController < ApplicationController
 
 def search
     @value = params["search"]["value"]         #データを代入
-    @datas = search_for(@value)          #def search_forを実行(引数に検索ワード)
+    @datas = Product.search_for(@value)          #def search_forを実行(引数に検索ワード)
 end
-
+=begin
   private
 
   def match(value)
-    Item.where(name: value)
+    Product.where(name: value)
   end
 
   def forward(value)                              #forward以降は商品名検索の定義しかしてません。
-    Item.where("name LIKE ?", "#{value}%")        
+    Product.where("name LIKE ?", "#{value}%")
   end
 
   def backward(value)
-    Item.where("name LIKE ?", "%#{value}")
+    Product.where("name LIKE ?", "%#{value}")
   end
 
   def partical(value)
-    Item.where("name LIKE ?", "%#{value}%")
+    Product.where("name LIKE ?", "%#{value}%")
   end
 
 
   def search_for(value)
     when 'match'
       match(value)
+    end
     when 'forward'
       forward(value)
+    end
     when 'backward'
       backward(value)
     when 'partical'
       partical(value)
     end
   end
+
+=end
 end
+
