b0c2561bdfc8cb660bcafb23a41c9c3fccdf7637
ranranluu <ranranluutennis@gmail.com>
2021-07-26 03:31:50 +0000

    [fix]

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -1,21 +1,21 @@
 class Public::OrdersController < ApplicationController
  before_action :authenticate_customer!
- 
+
  def new
   @order = Order.new
   @deliveries = Delivery.all
  end
- 
+
  def confirm
   @carts = Cart.where(customer_id: current_customer)
   @order = Order.new
   @order.payment_method = params[:order][:payment_method].to_i
- 
+
   if params[:order][:addresses] == "address"#登録住所（デフォルト）
   @order.postcode = current_customer.postcode
   @order.address = current_customer.address
-  @order.name = current_customer.last_name, current_customer.first_name
- 
+  @order.name = current_customer.last_name + current_customer.first_name
+
   elsif params[:order][:addresses] == "registration_adderss"#登録済み住所
   @oreder.postcode = Delivery.find(params[:order][:address_id]).postcode
   @oreder.address =  Delivery.find(params[:order][:address_id]).address
@@ -28,32 +28,32 @@ class Public::OrdersController < ApplicationController
 
   end
  end
- 
+
  def complete
  end
- 
+
  def create
   @order = current_customer.orders.new(order_params)
   @order.save
   redirect_to complete_path#confirmで注文確定後complete画面へ移動
  end
- 
+
  def index
   @orders = current_customer.orders
  end
- 
+
  def show
   @orders = Order.find(params[:id])
   @carts = Cart.where(customer_id: current_customer)
  end
- 
+
  def order_params
   params.require(:order).permit(:postage, :payment, :payment_method, :name, :postcode, :address, :total_price, :order_status)
  end
- 
+
  def address_params
   params.require(:order).permit(:postcode, :address, :name)
  end
- 
- 
+
+
 end
