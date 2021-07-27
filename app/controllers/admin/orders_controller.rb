0bfb6777f9dc51613d33e6278294ba398e3647ad
iinowataru <wpfujz@gmail.com>
2021-07-27 06:21:15 +0000

    admin-order

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -13,14 +13,17 @@ class Admin::OrdersController < ApplicationController
   end
 
   def show
-
+   @order = Order.find(params[:id])
   end
 
-  def updated
+  def update
+   @order = Order.find(params[:id])
+   @order.update(order_params)
+   redirect_to admin_order_path(@order) 
   end
 
   private
   def order_params
-    params.require(:orders).permit(:order_status)
+    params.require(:order).permit(:order_status)
   end
 end
