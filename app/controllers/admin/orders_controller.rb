cf8db08d347b3db7ed20ffb2629c8561efe0d65a
iinowataru <wpfujz@gmail.com>
2021-07-27 03:56:47 +0000

    public-order修正

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -4,7 +4,8 @@ class Admin::OrdersController < ApplicationController
   def index
     case params[:order_sort]
     when "0"
-      @customer = Customer.find(params[:customer_id])
+      customer_id = Rails.application.routes.recognize_path(request.referer)[:id]
+      @customer = Customer.find(customer_id)
       @orders = @customer.orders
     else 
       @orders = Order.all
